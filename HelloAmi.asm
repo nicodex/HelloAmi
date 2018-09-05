3fa96f3796ddeb1ac4463ed8e93b411ea5cdfb0d
Nico Bendlin <nico@nicode.net>
2018-09-05 16:05:24 +0200

    HelloAmi source commented

--- HelloAmi.asm
+++ HelloAmi.asm
@@ -1,61 +1,94 @@
-; vasmm68k_mot -Fhunkexe -o HelloAmi -nosym HelloAmi.asm
-start:
+; vasmm68k_mot[_<HOST>] -Fhunkexe -pic -nosym -o HelloAmi HelloAmi.asm
+HelloAmi:
 		movem.l	d2/d3/d4/d5/a2/a6,-(sp)
 		moveq	#122,d3         ; ERROR_INVALID_RESIDENT_LIBRARY
-		moveq	#0,d4           ; WBstartup message
+		moveq	#0,d4           ; WBStartup message
 		moveq	#0,d5           ; new console handle
 		movea.l	(4).w,a6        ; AbsExecBase
-		movea.l	$114(a6),a2     ; ThisTask
-		tst.l	$AC(a2)         ; pr_CLI
+		; shortcut for exec.library/FindTask(0)
+		movea.l	$0114(a6),a2    ; ThisTask
+		tst.l	$00AC(a2)       ; pr_CLI
 		bne.b	.openDos
-		lea	$5C(a2),a0      ; pr_MsgPort
-		jsr	-$180(a6)       ; _LVOWaitPort
-		lea	$5C(a2),a0      ; pr_MsgPort
-		jsr	-$174(a6)       ; _LVOGetMsg
+		;
+		; the Workbench sends a WBStartup message to our
+		; message port on start - this message has to be
+		; read before any dos.library function is called
+		;
+		lea	$005C(a2),a0    ; pr_MsgPort
+		jsr	-$0180(a6)      ; _LVOWaitPort
+		lea	$005C(a2),a0    ; pr_MsgPort
+		jsr	-$0174(a6)      ; _LVOGetMsg
 		move.l	d0,d4
 .openDos:
+		;
+		; gain access to the "dos.library" (any version)
+		;
 		lea	.dosName(pc),a1
-		jsr	-$198(a6)       ; _LVOOldOpenLibrary
+		jsr	-$0198(a6)      ; _LVOOldOpenLibrary
 		tst.l	d0
 		beq.b	.replyMsg
 		movea.l	d0,a6
-		move.l	$A0(a2),d1      ; pr_COS
+		;
+		; use Output stream of our process (if assigned)
+		;
+		move.l	$00A0(a2),d1    ; pr_COS
 		bne.b	.writeTxt
+		;
+		; else request a new window with the CON handler
+		;
 		pea	.conSpec
 		move.l	(sp)+,d1
 		move.l	#1006,d2        ; MODE_NEWFILE
-		jsr	-$1E(a6)        ; _LVOOpen
+		jsr	-$001E(a6)      ; _LVOOpen
 		move.l	d0,d5
 		beq.b	.dosError
 		move.l	d0,d1
 .writeTxt:
+		;
+		; D3 has been choosen for the extended exit code
+		; to initialize it to zero (OK) while the result
+		; of the dos.library/Write is tested for success
+		;
 		pea	.textStr
 		move.l	(sp)+,d2
 		moveq	#.textEnd-.textStr,d3
-		jsr	-$30(a6)        ; _LVOWrite
+		jsr	-$0030(a6)      ; _LVOWrite
 		sub.l	d0,d3
 		beq.b	.closeCon
 .dosError:
-		move.l	$94(a2),d3      ; pr_Result2 (IoErr)
+		; shortcut for dos.library/IoErr()
+		move.l	$0094(a2),d3    ; pr_Result2
 .closeCon:
+		;
+		; wait 1.5 s and close a newly opened CON window
+		;
 		tst.l	d5
 		beq.b	.closeDos
-		moveq	#127,d1
-		jsr	-$C6(a6)        ; _LVODelay
+		moveq	#125,d1
+		jsr	-$00C6(a6)      ; _LVODelay
 		move.l	d5,d1
-		jsr	-$24(a6)        ; _LVOClose
+		jsr	-$0024(a6)      ; _LVOClose
 .closeDos:
+		;
+		; cleanup - conclude access to the "dos.library"
+		;
 		movea.l	a6,a1
 		movea.l	(4).w,a6        ; AbsExecBase
-		jsr	-$19E(a6)       ; _LVOCloseLibrary
+		jsr	-$019E(a6)      ; _LVOCloseLibrary
 .replyMsg:
+		;
+		; forbid task rescheduling if run from Workbench
+		; (automatically broken/permitted on return) and
+		; reply the WBStartup message that has been read
+		;
 		tst.l	d4
 		beq.b	.result
-		jsr	-$84(a6)        ; _LVOForbid
+		jsr	-$0084(a6)      ; _LVOForbid
 		movea.l	d4,a1
-		jsr	-$17A(a6)       ; _LVOReplyMsg
+		jsr	-$017A(a6)      ; _LVOReplyMsg
 .result:
-		move.l	d3,$94(a2)      ; pr_Result2
+		; dos.library/SetIoErr() requires DOS version 36
+		move.l	d3,$0094(a2)    ; pr_Result2
 		move.l	d3,d0
 		beq.b	.return
 		moveq	#20,d0          ; RETURN_FAIL
@@ -63,10 +96,12 @@ start:
 		movem.l	(sp)+,a6/a2/d5/d4/d3/d2
 		rts
 .conSpec:
+		; continued with .dosName as title to save bytes
+		; (1.x CON handlers need non-empty width/height)
 		dc.b	"CON:/10/200/50/"
 .dosName:
 		dc.b	"dos.library",0
 .textStr:
 		dc.b	"Hello World!",10
 .textEnd:
-		align	2
+	align	2
