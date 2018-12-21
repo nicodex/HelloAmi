96177f981505f40732bde22ed6199a8bf37baeb5
Nico Bendlin <nico@nicode.net>
2018-12-21 15:08:56 +0100

    fix LoadWB issues
    - scratch register has been used after DOS call
    + reset process references to avoid inheritance

--- C/LoadWB.asm
+++ C/LoadWB.asm
@@ -1,6 +1,6 @@
 ; vasmm68k_mot[_<HOST>] -Fhunkexe -pic -nosym -o LoadWB LoadWB.asm
 LoadWB:
-		movem.l	a6/a2/d5/d4/d3/d2,-(sp)
+		movem.l	d2-d5/a2-a6,-(sp)
 		moveq	#0,d5           ; WBStartup message
 		movea.l	(4).w,a6        ; AbsExecBase
 		movea.l	$0114(a6),a2    ; ThisTask
@@ -12,32 +12,50 @@ LoadWB:
 		jsr	-$0174(a6)      ; _LVOGetMsg
 		move.l	d0,d5
 		;TODO: support AROS (workbench.library instead of .task)
-		;TODO: avoid that Workbench process inherits CLI handles
 .startWB:
 		; avoid Alert(AN_Workbench!AG_OpenLib!AO_IconLib) on 1.x
 		lea	.icnName(pc),a1
 		jsr	-$0198(a6)      ; _LVOOldOpenLibrary
 		tst.l	d0
 		beq.b	.replyWB
-		movea.l	d0,a1
-		jsr	-$019E(a6)      ; _LVOCloseLibrary
+		; test if Workbench task module is present
 		lea	.wbtName(pc),a1
 		jsr	-$0060(a6)      ; _LVOFindResident
 		move.l	d0,d3
 		beq.b	.replyWB
-		pea	.wbtName
-		move.l	(sp)+,d1
-		moveq	#1,d2
-		lsr.l	#2,d3
-		addq.l	#(2+$001A)/4,d3 ; RT_SIZE (BPTR-aligned)
-		pea	(6144).w
-		move.l	(sp)+,d4
+		; open dos.library
 		lea	.dosName(pc),a1
 		jsr	-$0198(a6)      ; _LVOOldOpenLibrary
 		tst.l	d0
 		beq.b	.replyWB
 		movea.l	d0,a6
+		; clear references (avoid inheritance)
+		moveq	#-1,d1
+		movea.l	$00B8(a2),a3    ; pr_WindowPtr
+		move.l	d1,$00B8(a2)    ; pr_WindowPtr
+		moveq	#0,d1           ; NULL
+		movea.l	$00A4(a2),a4    ; pr_ConsoleTask
+		move.l	d1,$00A4(a2)    ; pr_ConsoleTask
+		movea.l	$00AC(a2),a5    ; pr_CLI
+		move.l	d1,$00AC(a2)    ; pr_CLI
+		jsr	-$007E(a6)      ; _LVOCurrentDir
+		move.l	d0,-(sp)
+		; create Workbench task
+		pea	.wbtName(pc)
+		move.l	(sp)+,d1
+		moveq	#1,d2
+		lsr.l	#2,d3
+		addq.l	#($001A+2)/4,d3 ; RT_SIZE (BPTR-aligned)
+		pea	(6144).w
+		move.l	(sp)+,d4
 		jsr	-$008A(a6)      ; _LVOCreateProc
+		; restore references
+		move.l	(sp)+,d1
+		jsr	-$007E(a6)      ; _LVOCurrentDir
+		move.l	a5,$00AC(a2)    ; pr_CLI
+		move.l	a4,$00A4(a2)    ; pr_ConsoleTask
+		move.l	a3,$00B8(a2)    ; pr_WindowPtr
+		; close dos.library
 		movea.l	a6,a1
 		movea.l	(4).w,a6        ; AbsExecBase
 		jsr	-$019E(a6)      ; _LVOCloseLibrary
@@ -49,7 +67,7 @@ LoadWB:
 		jsr	-$017A(a6)      ; _LVOReplyMsg
 .return0:
 		moveq	#0,d0           ; RETURN_OK
-		movem.l	(sp)+,d2/d3/d4/d5/a2/a6
+		movem.l	(sp)+,d2-d5/a2-a6
 		rts
 .icnName:
 		dc.b	"icon.library",0
