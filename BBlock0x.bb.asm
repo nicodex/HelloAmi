22cbf7601333bee4e6f8a48049890af347104f78
Nico Bendlin <nico@nicode.net>
2021-03-26 17:05:13 +0100

    BBlock0x v3.6
    - save another longword in boot block code
    - do not include project info/link by default
    - include different DOS versions in build script

--- BBlock0x.bb.asm
+++ BBlock0x.bb.asm
@@ -1,16 +1,25 @@
 ; vasmm68k_mot -Fbin -pic -m68000 -no-fpu -no-opt -o BBlock0x.bb BBlock0x.bb.asm
 
 	ifnd BBLOCK0X_DOSVER
-	; ID_DOS_DISK         'DOS',0
-	; ID_FFS_DISK         'DOS',1
-	; ID_INTER_DOS_DISK   'DOS',2
-	; ID_INTER_FFS_DISK   'DOS',3
-	; ID_FASTDIR_DOS_DISK 'DOS',4
-	; ID_FASTDIR_FFS_DISK 'DOS',5
+	; ID_DOS_DISK         'DOS\0'
+	; ID_FFS_DISK         'DOS\1'
+	; ID_INTER_DOS_DISK   'DOS\2'
+	; ID_INTER_FFS_DISK   'DOS\3'
+	; ID_FASTDIR_DOS_DISK 'DOS\4'
+	; ID_FASTDIR_FFS_DISK 'DOS\5'
+	; ID_LNFS_DOS_DISK    'DOS\6'
+	; ID_LNFS_FFS_DISK    'DOS\7'
 BBLOCK0X_DOSVER EQU 0
 	endif
+	ifnd BBLOCK0X_NOINFO
+BBLOCK0X_NOINFO EQU 1
+	endif
 	ifnd BBLOCK0X_CHKSUM
-BBLOCK0X_CHKSUM EQU ~((~$D85BE1EC)+BBLOCK0X_DOSVER)
+	ifeq BBLOCK0X_NOINFO
+BBLOCK0X_CHKSUM EQU ~((~$830A73EC)+BBLOCK0X_DOSVER)
+	else
+BBLOCK0X_CHKSUM EQU ~((~$74A147A1)+BBLOCK0X_DOSVER)
+	endif
 	endif
 
 BBlock0x:
@@ -51,7 +60,9 @@ BBlock0x:
 		; this is part of any standard BootBlock
 		; (return the dos.library init function)
 		;
-		lea	.dosName(pc),a1
+		lea	.library(pc),a1
+		pea	('dos.').l
+		move.l	(sp)+,-(a1)
 		jsr	-$0060(a6)      ; _LVOFindResident
 		tst.l	d0
 		movea.l	d0,a0
@@ -64,12 +75,13 @@ BBlock0x:
 		not.l	d0
 		movea.l	(sp)+,a1
 		rts
-.dosName:
-		dc.b	"dos.library",0
 .expName:
-		dc.b	"expansion.library",0
+		dc.b	"expansion."
+.library:
+		dc.b	"library",0
 	align	1
 BBInfo0x:
+	ifeq BBLOCK0X_NOINFO
 		dcb.b	2*512-(BBInfo0x-BBlock0x)-80,0
 BBINFO0X_INFTD EQU ((2*512-(BBInfo0x-BBlock0x))/1000)
 BBINFO0X_INFTM EQU ((2*512-(BBInfo0x-BBlock0x))-(1000*BBINFO0X_INFTD))
@@ -98,6 +110,9 @@ BBINFO0X_INFDM EQU (BBINFO0X_INFHM-(10*BBINFO0X_INFDD))
 		dc.b	" project at: htt"
 		dc.b	"ps://github.com/"
 		dc.b	"nicodex/HelloAmi"
+	else
+		dcb.b	2*512-(BBInfo0x-BBlock0x),0
+	endif
 
 	if *-BBlock0x-2*512
 		fail "Unexpected boot block size, check your code."
--- BBlock0x.bb.py
+++ BBlock0x.bb.py
@@ -51,22 +51,36 @@ class BootBlock(ctypes.BigEndianStructure):
 assert ctypes.sizeof(BootBlock) == TD_SECTOR * BOOTSECTS
 
 class BBlock0x:
-    _FILE = 'BBlock0x.bb'
-    _vasmm68k_mot = os.path.join('vasm', 'vasmm68k_mot')
+    _BASE = 'BBlock0x'
+    _VASM = os.path.join('vasm', 'vasmm68k_mot')
     @classmethod
-    def build(cls):
-        p = cls._FILE.replace('/', os.sep)
-        if not os.path.isfile(p):
-            if not os.path.isfile(cls._vasmm68k_mot):
-                for vasm in glob.glob(cls._vasmm68k_mot + '*'):
-                    if os.access(vasm, os.X_OK):
-                        cls._vasmm68k_mot = vasm
+    def basepath(cls):
+        return cls._BASE.replace('/', os.sep)
+    @staticmethod
+    def dosverc(ver):
+        return chr(ord('0') + ver)
+    @classmethod
+    def outpath(cls, ver):
+        o = cls.basepath()
+        if ver != 0:
+            o += '.dos' + cls.dosverc(ver)
+        return o + '.bb'
+    @classmethod
+    def build(cls, ver):
+        o = cls.outpath(ver);
+        if not os.path.isfile(o):
+            if not os.path.isfile(cls._VASM):
+                for p in glob.glob(cls._VASM + '*'):
+                    if os.access(p, os.X_OK):
+                        cls._VASM = p
                         break
             subprocess.check_call(
-                [cls._vasmm68k_mot, '-quiet',
+                [cls._VASM, '-quiet',
                 '-Fbin', '-pic', '-m68000', '-no-fpu', '-no-opt',
-                '-o', p, p + '.asm'])
-        with open(p, 'r+b') as f:
+                '-DBBLOCK0X_DOSVER=' + cls.dosverc(ver),
+                '-DBBLOCK0X_NOINFO=1',
+                '-o', o, cls.basepath() + '.bb.asm'])
+        with open(o, 'r+b') as f:
             b = BootBlock.from_rawio(f)
             c = b.checksum
             f.seek(0)
@@ -74,15 +88,19 @@ class BBlock0x:
             if c != b.checksum:
                 print('checksum updated: $%08X -> $%08X' % (c, b.checksum))
     @classmethod
-    def clean(cls):
-        if os.path.isfile(cls._FILE):
-            os.remove(cls._FILE)
+    def clean(cls, ver):
+        o = cls.outpath(ver);
+        if os.path.isfile(o):
+            os.remove(o)
 
 def main(argv):
-    if (len(argv) > 1) and (argv[1] in ('clean', 'rebuild')):
-        BBlock0x.clean()
+    r = range(0, 7 + 1)
+    if (len(argv) > 1) and (argv[1] in ('clean', 'rebuild', 'all')):
+        [BBlock0x.clean(v) for v in r]
     if (len(argv) <= 1) or (argv[1] in ('build', 'rebuild')):
-        BBlock0x.build()
+        BBlock0x.build(0)
+    if (len(argv) > 1) and (argv[1] in ('all')):
+        [BBlock0x.build(v) for v in r]
 
 if __name__ == '__main__':
     main(sys.argv)
